43880ff98e4e637eff3ce5e9117b1374dea520d8
lhmn <maingan.19042001@gmail.com>
2022-05-17 19:49:09 +0700

    update xu li su kien

--- app/src/main/java/hcmute/edu/vn/nhom07/foodyapp_group07/MainActivity.java
+++ app/src/main/java/hcmute/edu/vn/nhom07/foodyapp_group07/MainActivity.java
@@ -2,6 +2,7 @@ package hcmute.edu.vn.nhom07.foodyapp_group07;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,42 +13,59 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-//    Button signIn_btn, skip_btn;
-//    TextView txt_signUp;
+    Button signIn_btn, skip_btn;
+    TextView txt_signUp;
 
-    ListView lvMonAnTrongGioHang;
-    ArrayList<MonAnTrongGioHang> mangMonAnTrongGioHang;
+//    ListView lvMonAnTrongGioHang;
+//    ArrayList<MonAnTrongGioHang> mangMonAnTrongGioHang;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.payment_page);
+        setContentView(R.layout.activity_main);
 
-//        signIn_btn = (Button) findViewById(R.id.signIn_btn);
-//        skip_btn = (Button) findViewById(R.id.skip_btn);
-//        txt_signUp = (TextView) findViewById(R.id.txt_signUp);
+        signIn_btn = (Button) findViewById(R.id.signIn_btn);
+        skip_btn = (Button) findViewById(R.id.skip_btn);
+        txt_signUp = (TextView) findViewById(R.id.txt_signUp);
+
+        txt_signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignUp.class);
+                startActivity(intent);
+            }
+        });
+
+        signIn_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignIn.class);
+                startActivity(intent);
+            }
+        });
+
+        skip_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+//        lvMonAnTrongGioHang = (ListView) findViewById(R.id.listViewMonAnTrongGioHang);
+//        mangMonAnTrongGioHang = new ArrayList<MonAnTrongGioHang>();
 //
-//        txt_signUp.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
+//        mangMonAnTrongGioHang.add(new MonAnTrongGioHang(1,"Cơm ốp la xúc xích kim chi sốt Gojuchang", 50000));
+//        mangMonAnTrongGioHang.add(new MonAnTrongGioHang(2,"Kimbap chiên xù", 30000));
+//        mangMonAnTrongGioHang.add(new MonAnTrongGioHang(1,"Chả cá hàn quốc", 20000));
 //
-//            }
-//        });
-
-        lvMonAnTrongGioHang = (ListView) findViewById(R.id.listViewMonAnTrongGioHang);
-        mangMonAnTrongGioHang = new ArrayList<MonAnTrongGioHang>();
-
-        mangMonAnTrongGioHang.add(new MonAnTrongGioHang(1,"Cơm ốp la xúc xích kim chi sốt Gojuchang", 50000));
-        mangMonAnTrongGioHang.add(new MonAnTrongGioHang(2,"Kimbap chiên xù", 30000));
-        mangMonAnTrongGioHang.add(new MonAnTrongGioHang(1,"Chả cá hàn quốc", 20000));
-
-        MonAnTrongGioHangAdapter anAdapter = new MonAnTrongGioHangAdapter(
-                MainActivity.this,
-                R.layout.dong_mon_an_trong_gio_hang,
-                mangMonAnTrongGioHang
-        );
-
-        lvMonAnTrongGioHang.setAdapter(anAdapter);
+//        MonAnTrongGioHangAdapter anAdapter = new MonAnTrongGioHangAdapter(
+//                MainActivity.this,
+//                R.layout.dong_mon_an_trong_gio_hang,
+//                mangMonAnTrongGioHang
+//        );
+//
+//        lvMonAnTrongGioHang.setAdapter(anAdapter);
 
     }
 }
---
+++ app/src/main/java/hcmute/edu/vn/nhom07/foodyapp_group07/SignIn.java
@@ -0,0 +1,45 @@
+package hcmute.edu.vn.nhom07.foodyapp_group07;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SignIn extends AppCompatActivity {
+
+    TextView edtEmail,edtPassWord;
+    Button btnSignIn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        //Tiến hành ánh xạ
+        edtEmail = (EditText)findViewById(R.id.editTextEmail);
+        edtPassWord = (EditText)findViewById(R.id.editTextPassword);
+        btnSignIn = (Button)findViewById(R.id.buttonSignIn);
+
+        //Phát sinh sự kiện OnClick
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = "lhmn@gmail.com";
+                String password = "lhmn";
+                if(edtEmail.getText().toString().equals(email) && edtPassWord.getText().toString().equals(password)){
+                    Intent intent = new Intent(SignIn.this, HomeActivity.class);
+                    startActivity(intent);
+                }
+//                else{
+//                    Toast.makeText(getApplicationContext(),R.string.login_error, Toast.LENGTH_LONG).show();
+//                }
+            }
+        });
+    }
+}
